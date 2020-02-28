90fe2d292ecdb53a4180ae7b80ee2542237f027e
wsyyyyy <476134046@qq.com>
2020-02-28 21:46:32 +0800

    2020-2-28更新

--- src/main/java/flinkdb/PaperCreate.java
+++ src/main/java/flinkdb/PaperCreate.java
@@ -7,10 +7,13 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PaperCreate {
+
+    private static Object String;
 
     public static void main(String[] args) throws Exception {
 
@@ -54,8 +57,8 @@ public class PaperCreate {
                 env.createInput(hdIf);
         */
         long s1 = System.currentTimeMillis();
-        //String filePath = "/Users/sub/Desktop/Flink/四六级词频实验/近五年六级/2019年6月.txt";
-        String filePath = "/Users/sub/Desktop/120万单词.txt";
+        String filePath = "/Users/sub/Desktop/Flink/四六级词频实验/2013-2019六级/2019年6月1.txt";
+        //String filePath = "/Users/sub/Desktop/120万单词.txt";
         //DataSet<String> text = env.readTextFile(String.valueOf(input));
         DataSet<String> text = env.readTextFile(filePath);
         long s2 = System.currentTimeMillis();
@@ -66,15 +69,65 @@ public class PaperCreate {
                         .groupBy(0)
                         .sum(1)
                         .sortPartition(1, Order.DESCENDING).setParallelism(1)
+                        .filter(keyValue -> (int)keyValue.getField(1) == 5)
                 ;
+        DataSet<String> word = counts.flatMap(new WordSplitter());
+        word.writeAsText("/Users/sub/Desktop/word.txt ").setParallelism(1);
         long s3 = System.currentTimeMillis();
         // execute and print result
-        counts.writeAsText("/Users/sub/Desktop/Flink/四六级词频实验/六级词频/2013-2019六级总词频.txt ").setParallelism(1);
+        //counts.writeAsText("/Users/sub/Desktop/Flink/四六级词频实验/六级词频/试卷test3.txt ").setParallelism(1);
         counts.print();
+        word.print();
         long s4 = System.currentTimeMillis();
         System.out.println("读取文件时间："+(s3-s2)+"ms");
         System.out.println("处理时间："+(s4-s3)+"ms");
         System.out.println("总时间："+(s4-s2)+"ms");
+
+        //读文件，将文件中的数据存放到一个字符串中
+            String filePath1 = "/Users/sub/Desktop/Flink/四六级词频实验/2013-2019六级/2019年6月1.txt";
+            String filePath2 = "/Users/sub/Desktop/word.txt";
+            BufferedReader br = null;
+            BufferedReader br1 = null;
+            BufferedWriter bw = null;
+            try {
+                //定义一个字符串用于接收读取的数据
+                StringBuffer sb = new StringBuffer();
+                //获得输入流对象，可以读取文件
+                br = new BufferedReader(new FileReader(filePath1));
+                br1 = new BufferedReader(new FileReader(filePath2));
+                String line;
+                String W;
+                //循环逐行读取数据
+                while(null != (line = br.readLine())){
+                    sb.append(line).append("\n");
+                }
+                String s = sb.toString();
+                while(null != (W = br1.readLine())){
+                    s = s.replaceAll("[^a-zA-Z]"+W+"[^a-zA-Z]", " ___(Q)___ ");
+                }
+                System.out.println(s);
+                //创建一个输出流对象
+                bw = new BufferedWriter(new FileWriter("/Users/sub/Desktop/试卷test3.txt "));
+                bw.write(s);
+                bw.flush();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }finally{
+                try {
+                    if(null != br){
+                        br.close();
+                    }
+                    if(null != br1){
+                        br1.close();
+                    }
+                    if(null != bw){
+                        bw.close();
+                    }
+                }catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
 
     }
 
@@ -144,6 +197,52 @@ public class PaperCreate {
                     out.collect(new Tuple2<String, Integer>(token, 1));
                 }
             }
+        }
+    }
+    public static final class WordSplitter implements FlatMapFunction<Tuple2<String, Integer>, String> {
+        @Override
+        public void flatMap(Tuple2<String, Integer> value, Collector<String> out) {
+            String tokens = value.f0;
+            /*for (String token : tokens) {
+                System.out.println(token);
+            }*/
+            //读文件，将文件中的数据存放到一个字符串中
+            /*String filePath = "/Users/sub/Desktop/Flink/四六级词频实验/2013-2019六级/2019年6月1.txt";
+            BufferedReader br = null;
+            BufferedWriter bw = null;
+            try {
+                //定义一个字符串用于接收读取的数据
+                StringBuffer sb = new StringBuffer();
+                //获得输入流对象，可以读取文件
+                br = new BufferedReader(new FileReader(filePath));
+                String line;
+                //循环逐行读取数据
+                while(null != (line = br.readLine())){
+                    sb.append(line + "\n");
+                }
+                String s = sb.toString();
+                s = s.replaceAll(value.getField(0), "_____");
+                System.out.println(s);
+                //创建一个输出流对象
+                bw = new BufferedWriter(new FileWriter("/Users/sub/Desktop/试卷test3.txt "));
+                bw.write(s);
+                bw.flush();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }finally{
+                try {
+                    if(null != br){
+                        br.close();
+                    }
+                    if(null != bw){
+                        bw.close();
+                    }
+                }catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }*/
+            // emit the words
+                    out.collect(value.getField(0));
         }
     }
 }
